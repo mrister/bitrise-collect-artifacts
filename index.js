be5157dfc0a894974bce46c014b337b01b91e71a
mrister <mihovil.rister@gmail.com>
2020-08-17 16:39:08 +0200

    feature: Do requests with retry attempts.

--- index.js
+++ index.js
@@ -1,59 +1,70 @@
-const request = require("request");
-const https = require("https");
-const fs = require("fs");
+const request = require('requestretry')
+const unusedFilename = require('unused-filename');
+const https = require('https')
+const fs = require('fs')
 
-const APP_SLUG = process.env.BITRISE_APP_SLUG;
-const BUILD_SLUGS = process.env.buildslugs.split("\n");
-const API_KEY = process.env.access_token;
-const SAVE_PATH = process.env.save_path;
+const APP_SLUG = process.env.BITRISE_APP_SLUG
+const BUILD_SLUGS = process.env.buildslugs.split('\n')
+const API_KEY = process.env.access_token
+const SAVE_PATH = process.env.save_path
+const MAX_ATTEMPTS = process.env.max_attempts
+const RETRY_DELAY = process.env.retry_delay
+const REQUEST_TIMEOUT = process.env.request_timeout
 
-console.log("APP_SLUG:",APP_SLUG);
-console.log("BUILD_SLUGS:",BUILD_SLUGS);
-const BASE_URL = "https://api.bitrise.io/v0.1/apps/";
-const BUILD_URL = BASE_URL + APP_SLUG + "/builds/";
+console.log('APP_SLUG:', APP_SLUG)
+console.log('BUILD_SLUGS:', BUILD_SLUGS)
+const BASE_URL = 'https://api.bitrise.io/v0.1/apps/'
+const BUILD_URL = BASE_URL + APP_SLUG + '/builds/'
 
-for(let i=0; i<BUILD_SLUGS.length; i++){
-  const url = BUILD_URL+BUILD_SLUGS[i];
+for (let i = 0; i < BUILD_SLUGS.length; i++) {
+  const url = BUILD_URL + BUILD_SLUGS[i]
   let options = {
-    "method": "GET",
-    "url": url,
-    "headers": {
-      "accept": "application/json",
-      "Authorization": API_KEY
+    method: 'GET',
+    url: url,
+    timeout: parseInt(REQUEST_TIMEOUT, 10) || 120000,
+    maxAttempts: parseInt(MAX_ATTEMPTS, 10) || 3,
+    retryDelay: parseInt(RETRY_DELAY, 10) || 3500,
+    retryStrategy: request.RetryStrategies.HTTPOrNetworkError,
+    'headers': {
+      'accept': 'application/json',
+      'Authorization': API_KEY
     }
-  };
+  }
 
-  request(options, function (error, response) { 
-    if (error) throw new Error(error);
-    const buildStatus = JSON.parse(response.body).data;
-    if(buildStatus && buildStatus.status != 0){
-      options.url = url + "/artifacts";
+  request(options, function (error, response) {
+    if (error) throw new Error(error)
+    const buildStatus = JSON.parse(response.body).data
+    console.info('buildStatus', buildStatus)
+    if (buildStatus && buildStatus.status != 0) {
+      options.url = url + '/artifacts'
 
       // Get Build Artifacts
-      request(options, function (error, response) { 
-        if (error) throw new Error(error);
-        const artifactsObj = JSON.parse(response.body).data;
-        if(artifactsObj){
+      request(options, function (error, response) {
+        if (error) throw new Error(error)
+        const artifactsObj = JSON.parse(response.body).data
+        if (artifactsObj) {
           artifactsObj.forEach((artifact) => {
-            options.url = url + "/artifacts/" + artifact.slug;
+            options.url = url + '/artifacts/' + artifact.slug
 
             // Get Build Artifact
-            request(options, (error, response) => { 
-              if (error) throw new Error(error);
-              const artifactObj = JSON.parse(response.body).data;
-              if(artifactObj){
-                console.log("Artifact URL:", artifactObj.expiring_download_url);
-                const file = fs.createWriteStream(SAVE_PATH+artifactObj.title);
+            request(options, (error, response) => {
+              if (error) throw new Error(error)
+              const artifactObj = JSON.parse(response.body).data
+              if (artifactObj) {
+                console.log('Artifact URL:', artifactObj.expiring_download_url)
+                const file = fs.createWriteStream(unusedFilename.sync(SAVE_PATH + artifactObj.title))
                 https.get(artifactObj.expiring_download_url, (res) => {
-                 res.pipe(file);
-                });
+                  res.pipe(file)
+                })
               }
             })
-          });
+          })
+        } else {
+          console.warn('No artifactsObj')
         }
-      });
+      })
     } else {
-      console.log("Error Bitrise API:", response.body);
+      console.error('Error Bitrise API:', response.body)
     }
-  });
+  })
 }
--- step.sh
+++ step.sh
@@ -4,12 +4,13 @@ set -e
 TMP_CURRENT_DIR="$( pwd )"
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# shellcheck disable=SC2154
 envman add --key BITRISE_ARTIFACTS_SAVE_PATH --value $save_path
 
 cd $THIS_SCRIPT_DIR
 
-echo '$' "npm i request"
-npm i request
+echo '$' "npm i"
+npm i
 
 echo '$' "node "$THIS_SCRIPT_DIR/index.js""
 node "$THIS_SCRIPT_DIR/index.js"
